8691ba470b4ad2a734e6405739cd13e707d07d41
ajerni <ajerni@gmail.com>
2021-06-06 12:51:30 +0200

    init

--- src/hello/main.mo
+++ src/hello/main.mo
@@ -1,4 +1,16 @@
 actor {
+   
+   stable var wert : Text = "Mein initial Wert."; //stable erhält den aktuelln Wert im canister bei upgrade
+
+    let myText = " ...has been saved.";
+
+    public func getWert() : async Text { return wert };
+
+    public func setWert(newwert : Text) : async Text{
+        wert := newwert; // := um mutable var anzupassen --> dfx canister call hello setWert '("Mein Text")'
+        return wert # myText;
+    };
+
     public func greet(name : Text) : async Text {
         return "Hello, " # name # "!";
     };
--- src/hello_assets/src/index.js
+++ src/hello_assets/src/index.js
@@ -1,12 +1,26 @@
-import { Actor, HttpAgent } from '@dfinity/agent';
-import { idlFactory as hello_idl, canisterId as hello_id } from 'dfx-generated/hello';
+import { Actor, HttpAgent } from "@dfinity/agent";
+import {
+  idlFactory as hello_idl,
+  canisterId as hello_id,
+} from "dfx-generated/hello";
 
 const agent = new HttpAgent();
 const hello = Actor.createActor(hello_idl, { agent, canisterId: hello_id });
+
+document.addEventListener("DOMContentLoaded", async () => {
+  document.getElementById("showwert").innerText = await hello.getWert();
+});
 
 document.getElementById("clickMeBtn").addEventListener("click", async () => {
   const name = document.getElementById("name").value.toString();
   const greeting = await hello.greet(name);
 
   document.getElementById("greeting").innerText = greeting;
+});
+
+document.getElementById("clickMeBtn2").addEventListener("click", async () => {
+  const wert = document.getElementById("wert").value.toString();
+  document.getElementById("showwert").innerText = "...saving new state...";
+  const newwert = await hello.setWert(wert);
+  document.getElementById("showwert").innerText = newwert;
 });
